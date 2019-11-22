f63449f892cdba8081821ca47e68717c39e91782
dugang <grant_reg@163.com>
2019-11-22 18:09:20 +0800

    新增regexTest
    Signed-off-by: dugang <grant_reg@163.com>

---
+++ firstPython/regexTest.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019/11/22 16:31
+# @Author : ZarinMaster
+# @Site : 
+# @File : regexTest.py
+# @Software: PyCharm
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
